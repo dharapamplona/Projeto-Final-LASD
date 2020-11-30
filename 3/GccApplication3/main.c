16cc7a1573e6b4769333c41c0ff058967ff37e54
dharapamplona <dharamaria2008@hotmail.com>
2020-11-30 14:31:06 -0300

    versão 3
    código comentado

---
+++ 3/GccApplication3/main.c
@@ -0,0 +1,112 @@
+/*
+ * GccApplication3.c
+ 
+ * Author : Dhara Pamplona
+ */ 
+
+#include <avr/io.h>
+#include "i2c.h" // biblioteca necessária para comunicção com LCD
+#include "LCD_I2C.h" // biblioteca que permite a programação do LCD
+#define F_CPU 16000000UL //Frequência de trabalho da CPU
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#define tam_vetor 3 
+
+unsigned char leitura_ADC_string[tam_vetor]; // recebe o tamanho 3 que é quantidade de digitos maxima 
+unsigned char dutycycle_string[tam_vetor]; // recebe o tamanho 3 que é quantidade de digitos maxima
+
+uint16_t leitura_ADC = 0; // variavel que está sendo associada ao sensor de temperatura
+uint8_t dutycycle = 0;  // responsavel por receber o valor da temperatura e mostrar a % de quão quente está a placa 
+
+
+ISR(ADC_vect)
+{
+	leitura_ADC = ADC; // ADC vai ta constantemente recebendo os valores de enviados por leitura_ADC
+}
+
+
+//Conversão de inteiro para string
+
+void int2string(unsigned int valor,unsigned char *disp)
+{
+	// funçaõ que converte o valor para char 
+	
+	unsigned int unidade = valor %10;
+	unsigned int dezena  = (valor /10)%10;
+	unsigned int centena = (valor/100)%10;
+	*(disp + 3) = 0 ; // 0 é nada em char 
+	*(disp + 2) = unidade +  48 ; // soma zero(que em char é representado por 48) ao valor valor
+	*(disp + 1) = dezena  +  48 ; // soma zero(que em char é representado por 48) ao valor valor
+	*(disp + 0) = centena +  48 ; // soma zero(que em char é representado por 48) ao valor valor
+	
+}
+
+ // essa função é responsavel por travar o valor em 100 %
+ 
+uint8_t limitante(uint16_t numero, uint8_t maxi)
+{
+	if (numero <= maxi){
+		return numero;
+	}
+	else {
+		return maxi;
+	}	
+}
+
+
+int main(void)
+{
+	
+    DDRD = 0xFF; //Porta D como saída
+    DDRC = 0x00; //Porta C como entrada
+    PORTC = 0xFE; //Desabilita o pullup do PC0
+	
+	ADMUX = 0b01000000; // refência definida por har.
+	
+	ADCSRA = 0b11101111; //habilita o AD, habilita interrupção, modo de conversão contínua, prescaler = 128
+	ADCSRB = 0x00; //modo de conversão contínua
+	DIDR0 = 0b00001110; //habilita pino PC0 como entrada do ADC0
+	TCCR0A = 0b10100011; //pwm rapido, modo de comparaçao, modo de funcionamento
+	TCCR0B = 0b00000101; //1024, frequencia de 61hz
+	
+	sei();
+	
+	i2c_init(); // inicializa 
+	i2c_start(); //att 
+	i2c_write(0x70); // 
+	lcd_init(); //
+	
+	
+	lcd_cmd(0x80); // escolhendo o local que será escrita uma mensagem
+	lcd_msg("Temperatura:"); // mensagem 
+	
+	lcd_cmd(0x8F); // escolhendo o local que será escrita uma mensagem
+	lcd_msg("C"); // mensagem
+	
+	lcd_cmd(0xCF); // escolhendo o local que será escrita uma mensagem
+	lcd_msg("%");// mensagem
+	
+	lcd_cmd(0xC0); // escolhendo o local que será escrita uma mensagem
+	lcd_msg("Vel. Motor:");// mensagem
+	
+    while (1) 
+    {
+		
+		dutycycle = limitante ((leitura_ADC*1.554878049),255); 
+		
+		int2string (leitura_ADC*0.487,leitura_ADC_string);
+		lcd_cmd(0x8C); // escolhendo o local que será escrita uma mensagem 
+		lcd_msg(leitura_ADC_string);// mensagem
+		
+		int2string (dutycycle*0.3921568627,dutycycle_string);
+		lcd_cmd(0xCC); // escolhendo o local que será escrita uma mensagem
+		lcd_msg(dutycycle_string);// mensagem
+
+		
+		
+		
+    }
+}
+
+//1,5v = 307 (máximo)
+//duty = leitura_adc * 0.83 *0.3921568627
